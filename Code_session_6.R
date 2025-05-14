1ecf924752dd9df45bd67956298e8e402892e421
Kevin Koehler <kevin.koehler@santannapisa.it>
2025-05-14 11:54:37 +0200

    Update Code_session_6.R

--- Code_session_6.R
+++ Code_session_6.R
@@ -15,3 +15,17 @@ sumstats <- data %>%
          max=round(max,2))
 
 write_csv(sumstats, "summary_statistic.csv")
+
+
+data <- data %>%
+  mutate(
+    positive=rowMeans(cbind(pos1, pos2, pos3), na.rm = T),
+    negative=rowMeans(cbind(neg1, neg2, neg3), na.rm = T),
+    fp_views=(positive-negative)/100)
+
+data <- data %>%
+  mutate(s1=pos1-neg1,
+         s2=pos2-neg2,
+         s3=pos3-neg3,
+         fp_views=rowMeans(cbind(s1,s2,s3), na.rm=T))
+
