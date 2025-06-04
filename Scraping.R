3f5553507f0c62b1956902e80c68ed8f0fc3e4a9
Kevin Koehler <kevin.koehler@santannapisa.it>
2025-06-04 19:37:43 +0200

    Update Scraping.R

--- Scraping.R
+++ Scraping.R
@@ -3,3 +3,9 @@ url <- "https://en.wikipedia.org/wiki/List_of_presidents_of_the_United_States"
 html <- read_html(url)
 
 us_table <- html %>% html_elements(".wikitable") %>% html_table() %>% as.data.frame()
+
+us_table <- us_table %>% 
+  select(Name.birth.death., Term.16, Party.b..17..1) %>%
+  rename(name=Name.birth.death.,
+         term=Term.16,
+         party=Party.b..17..1)
