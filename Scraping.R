98c2df003c6f77ce9ae77c0f77101b925d6db639
Kevin Koehler <kevin.koehler@santannapisa.it>
2025-06-04 19:29:33 +0200

    Update Scraping.R

--- Scraping.R
+++ Scraping.R
@@ -2,4 +2,4 @@ library(tidyverse)
 url <- "https://en.wikipedia.org/wiki/List_of_presidents_of_the_United_States"
 html <- read_html(url)
 
-us_table <- html %>% 
+us_table <- html %>% html_elements(".wikitable") %>% html_table()
